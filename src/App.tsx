c5be756e4b233fae70b4e79b8f4dd49a3d509895
andre <anddreluis98@gmail.com>
2022-12-16 20:46:05 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import Header from "./pages/header";
+
+export default function App() {
+  return <Header />;
+}
---
+++ src/pages/header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <>
+      <h1 className="flex text-3xl justify-center text-red-700">Salve!</h1>;
+    </>
+  );
+}
