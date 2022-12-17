da3f51d83c53586cdd5f7e98e58676b627611d25
anddreluis2 <anddreluis98@gmail.com>
2022-12-16 22:49:26 -0300

    created application header.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,26 @@
+import Logo from "./logo";
+import { FaAngleDown } from "react-icons/fa";
+
 export default function Header() {
   return (
-    <>
-      <h1 className="flex text-3xl justify-center text-red-700">Salve!</h1>;
-    </>
+    <div className="flex text-white h-16 items-center bg-purple p-6">
+      {/*Logo*/}
+      <span className="hover:cursor-pointer">
+        <Logo />
+      </span>
+
+      {/*Paths*/}
+      <div className="text-white flex gap-10 text-lg font-medium ml-6">
+        <h5 className="flex items-center gap-2">
+          Comunicação <FaAngleDown />
+        </h5>
+        <h5 className="flex items-center gap-2">
+          Gestão <FaAngleDown />
+        </h5>
+        <h5 className="flex items-center gap-2">
+          Cobrança <FaAngleDown />
+        </h5>
+      </div>
+    </div>
   );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      purple: '#6600CC',
+      white: '#FFFFFF',
+      grayHigh: '#444648',
+    },
     extend: {},
   },
   plugins: [],
